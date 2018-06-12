fb3b062489f83453513fcdf2782d2654b0783a21
刘兆坤 <zhaokun_liu@163.com>
2018-06-12 13:35:19 +0800

    Add files via upload

---
+++ 17-数据库.py
@@ -0,0 +1,46 @@
+# import sqlite3
+# conn=sqlite3.connect('test.db')
+# cursor=conn.cursor()
+# # cursor.execute('Create table user(id varchar(20) primary key,name varchar(20))')
+# # cursor.execute('insert into user(id,name) values(\'1\',\'Michael\')')
+# # print(cursor.rowcount)
+# # cursor.close()
+# # conn.commit()
+# # conn.close()
+# cursor.execute('select * from user where id=?',('1',))
+# values=cursor.fetchall()
+# print(values)
+# cursor.close()
+# conn.close()
+# -*- coding: utf-8 -*-
+
+import os, sqlite3
+
+db_file = os.path.join(os.path.dirname(__file__), 'test.db')
+if os.path.isfile(db_file):
+    os.remove(db_file)
+
+# 初始数据:
+conn = sqlite3.connect(db_file)
+cursor = conn.cursor()
+cursor.execute('create table user(id varchar(20) primary key, name varchar(20), score int)')
+cursor.execute(r"insert into user values ('A-001', 'Adam', 95)")
+cursor.execute(r"insert into user values ('A-002', 'Bart', 62)")
+cursor.execute(r"insert into user values ('A-003', 'Lisa', 78)")
+cursor.close()
+conn.commit()
+conn.close()
+
+def get_score_in(low, high):
+	with sqlite3.connect(db_file)as conn:
+		cursor=conn.cursor()
+		cursor.execute('select name from user WHERE score>=? AND score<=? order by score',(low,high))
+		values=cursor.fetchall()
+		cursor.close()
+	return [x[0] for x in values]	
+# 测试:
+assert get_score_in(80, 95) == ['Adam'], get_score_in(80, 95)
+assert get_score_in(60, 80) == ['Bart', 'Lisa'], get_score_in(60, 80)
+assert get_score_in(60, 100) == ['Bart', 'Lisa', 'Adam'], get_score_in(60, 100)
+
+print('Pass')
